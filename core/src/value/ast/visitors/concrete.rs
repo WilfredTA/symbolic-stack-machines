639420c956453405f76eaf76df724d6653d2a110
Tannr <tannr@hey.com>
2022-06-28 23:16:11 -0400

    Add Final hook, simplify pre and post hooks in interpreter

--- core/src/value/ast/visitors/concrete.rs
+++ core/src/value/ast/visitors/concrete.rs
@@ -10,33 +10,67 @@ pub struct Interpreter {
     mode: InterpreterMode,
 }
 
-// Post hook is called once after all processing is finished
+// Post hook is called after the AST has been processed
 // Pre hook is called at each node in the AST prior to potentially increased traversal.
 // If pre_hook returns None, then processing continues as normal. If it returns Some(Sentence), then the sentence returned
 // is used instead of the sentence that would result from further processing.
+// Final is only called at the end
 impl Interpreter {
-    pub fn interpret<T, Pre, Post>(&self, pre_hook: Pre, post_hook: Post) -> T
+    pub fn interpret<T, Hook, Final>(&self, pre_hook: Hook, post_hook: Hook, final_hook: Final) -> T
     where 
-        Pre: Fn(Sentence) -> Option<Sentence>,
-        Post: Fn(Sentence) -> T,
+        Hook: Fn(Sentence) -> Option<Sentence>,
+        Final: Fn(Sentence) -> T,
     {
-       let sentence = self.process_sentence(&self.pgm);
-       post_hook(sentence)
+       if let Some(s) = pre_hook(self.pgm.clone()) {
+           let ret = match self.exec_hook(&post_hook, &s) {
+               Some(sen) => sen,
+               None => s
+           };
+           final_hook(ret)
+       } else {
+           let sentence = self.process_sentence::<Hook>(&self.pgm, &pre_hook, &post_hook);
+           let ret = match self.exec_hook(&post_hook, &sentence) {
+                Some(sen) => sen,
+                None => sentence
+            };
+            final_hook(ret)
+       }
+      
     }
 
-    pub fn visit(&self) -> Sentence {
-        self.process_sentence(&self.pgm)
+
+    fn exec_hook(&self, hook: &impl Fn(Sentence) -> Option<Sentence>, on: &Sentence) -> Option<Sentence> {
+        hook(on.clone())
     }
-    
-    fn process_sentence(&self, s: &Sentence) -> Sentence {
-        match s {
-            Sentence::BinOp { a, b, op } => {
-                self.process_bin_op(&a.inner(), &b.inner(), op.clone())
-            }
-            Sentence::UnaryOp { a, op } => todo!(),
-            Sentence::TernaryOp { a, b, c, op } => todo!(),
-            Sentence::Basic(v) => s.clone(),
+
+
+    fn process_sentence<Hook>(&self, s: &Sentence, pre_hook: &Hook, post_hook: &Hook) -> Sentence 
+    where 
+        Hook: Fn(Sentence) -> Option<Sentence>,
+    {
+  
+       let res =  match self.exec_hook(pre_hook, s) {
+            None => {
+                match s {
+                    Sentence::BinOp { a, b, op } => {
+                       self.process_bin_op::<Hook>(&a.inner(), &b.inner(), op.clone(), pre_hook, post_hook)
+                       
+                    }
+                    Sentence::UnaryOp { a, op } => todo!(),
+                    Sentence::TernaryOp { a, b, c, op } => todo!(),
+                    Sentence::Basic(v) => s.clone(),
+                }
+            },
+            Some(s) => s
+        };
+
+        // We only need to exec post_hook here , since every node in the tree is encapsulated by a sentence, so each subtree traversal
+        // will result in a call to process_sentence
+        match self.exec_hook(post_hook, &res) {
+            Some(s) => s,
+            None => res
         }
+        
     } 
 
     fn get_basic_val(s: &Sentence) -> Option<Value> {
@@ -46,28 +80,50 @@ impl Interpreter {
             None
         }
     }
-    fn process_bin_op(&self, a: &Sentence, b: &Sentence, op: BinOp) -> Sentence {
+
+
+
+    fn process_bin_op<Hook>(&self, a: &Sentence, b: &Sentence, op: BinOp, pre_hook: &Hook, post_hook: &Hook) -> Sentence 
+    where 
+        Hook: Fn(Sentence) -> Option<Sentence>,
+    {
+   
         match op {
             BinOp::Plus => {
-                let aa = self.process_sentence(a);
-                let bb = self.process_sentence(b);
+
+                let aa = match self.exec_hook(pre_hook, a) {
+                  None => self.process_sentence::<Hook>(a, pre_hook, post_hook),
+                  Some(s) => s  
+                };
+                let bb = match self.exec_hook(pre_hook, b) {
+                    None => self.process_sentence::<Hook>(b, pre_hook, post_hook),
+                    Some(s) => s  
+                };
+
+                
+                Sentence::BinOp { a: Val::new(aa), b: Val::new(bb), op: BinOp::Plus }
                 // These should always return Some(Value) since the above calls should reduce them
-                let aa_basic = Interpreter::get_basic_val(&aa).unwrap();
-                let bb_basic = Interpreter::get_basic_val(&bb).unwrap();
-                match (aa_basic, bb_basic) {
-                    (Value::Symbolic(x), Value::Symbolic(y)) => {
-                        todo!()
-                    },
-                    (Value::Symbolic(x), Value::Concrete(y)) => {
-                        todo!()
-                    },
-                    (Value::Concrete(x), Value::Symbolic(y)) => {
-                        todo!()
-                    },
-                    (Value::Concrete(x), Value::Concrete(y)) => {
-                        todo!()
-                    },
-                }
+                
+
+                // The below could be the skeleton of a post_hook that adds numbers together to
+                // simplify the BinOp to a single BasicValue
+                
+                //let aa_basic = Interpreter::get_basic_val(&aa).unwrap();
+                //let bb_basic = Interpreter::get_basic_val(&bb).unwrap();
+                // match (aa_basic, bb_basic) {
+                //     (Value::Symbolic(x), Value::Symbolic(y)) => {
+                //         todo!()
+                //     },
+                //     (Value::Symbolic(x), Value::Concrete(y)) => {
+                //         todo!()
+                //     },
+                //     (Value::Concrete(x), Value::Symbolic(y)) => {
+                //         todo!()
+                //     },
+                //     (Value::Concrete(x), Value::Concrete(y)) => {
+                //         todo!()
+                //     },
+                // }
 
             },
             BinOp::Minus => todo!(),
